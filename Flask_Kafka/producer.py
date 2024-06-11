5c53e0f502143cd60fabcb3cef167e7266ab7771
hash2004 <151638487+hash2004@users.noreply.github.com>
2024-06-12 03:23:10 +0500

    Update producer.py

--- Flask_Kafka/producer.py
+++ Flask_Kafka/producer.py
@@ -6,17 +6,14 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-# Initialize Kafka producer
 p = Producer({'bootstrap.servers': 'localhost:9092'})
 
 @app.route('/result', methods=['GET','POST'])
 def result():
     if request.method == 'POST':
-        # ... your existing code ...
 
         top_5_asin_list = [{"asin_index": row.asin_index} for row in top_5_asin]
 
-        # Produce and send the top 5 ASINs to Kafka
         p.produce('test', value=json.dumps({"top_5_asin": top_5_asin_list}), callback=delivery_report)
         p.flush()
 
